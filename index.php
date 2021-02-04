533fc1abfe4b3015bfcb51e221624d26e9b069f9
Artem Chirtsov <chirartem@gmail.com>
2021-02-04 18:05:55 +0300

    .

--- index.php
+++ index.php
@@ -17,12 +17,13 @@ if ($conn->connect_error) {
 }
 
 ?>
-<!DOCTYPE html> <!-- Эта версия будет разрабатываться с PHP!!! -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ru">
 <head>
 	<meta charset="UTF-8" />
 	<title>Интересные растения</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 </head>
 <body>
 	<p class="logotyp">
@@ -32,39 +33,32 @@ if ($conn->connect_error) {
 			<span>Amazing <sub>plants</sub></span>
 		</a>
 	</p>
-	<div class="border">
+	<div class="menu">
 		<h2>Растения:</h2>
 		<ul>
 
-<?php
-$sql = "SELECT name from plants where visible=1 order by sort";
-$result = $conn->query($sql);
+			<?php
+			$sql = "SELECT name from plants where visible=1 order by sort";
+			$result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()) {
-	echo "<li><a href=\"\">".$row["name"]."</a></li>";
-} 
-?>
+			while($row = $result->fetch_assoc()) {
+				echo "<li><a href=\"\">".$row["name"]."</a></li>";
+			} 
+			?>
 		</ul>
 		<h2>Деревья:</h2>
 		<ul>
 
-<?php
-$sql = "SELECT name from trees where visible=1 order by sort";
-$result = $conn->query($sql);
+			<?php
+			$sql = "SELECT name from trees where visible=1 order by sort";
+			$result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()) {
-	echo "<li><a href=\"\">".$row["name"]."</a></li>";
-} 
-?>
+			while($row = $result->fetch_assoc()) {
+				echo "<li><a href=\"\">".$row["name"]."</a></li>";
+			} 
+			?>
 
 		</ul>
 	</div>
-	<br />
-	<br />
-	<br />
-	
-<!-- span:hover {
-	 color: white;
-	} -->
 </body>
 </html>
