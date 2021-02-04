62d984b85a08dcb8c971ad5949e43eb37bbc1ac7
Artem Chirtsov <chirartem@gmail.com>
2021-02-04 21:50:37 +0300

    test

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($conn->connect_error) {
 <html lang="ru">
 <head>
 	<meta charset="UTF-8" />
-	<title>Интересные растения </title>
+	<title>Интересные растения</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 </head>
