2007baf507931fae246870784ac0a6b0535a8db6
Artem Chirtsov <chirartem@gmail.com>
2021-02-07 22:11:18 +0300

    +

--- index.php
+++ index.php
@@ -85,11 +85,10 @@ if ($conn->connect_error) {
 			$link_type = $_GET["type"];
 			$link_id = $_GET["id"];
 			$sql = "SELECT name from $link_type where visible=1 and id=$link_id";
-			echo $sql;
 			$result = $conn->query($sql);
 
-            var_dump($result->fetch_assoc());
-            echo $row;
+            $result->fetch_assoc();
+            echo $row["name"];
 			?>
 		</div>
 	</div>
