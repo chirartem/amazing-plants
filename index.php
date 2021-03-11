5925be68acc4b6858bb24c0fa6929355bac1f040
Artem Chirtsov <chirartem@gmail.com>
2021-03-11 15:17:15 +0300

    +lang="ru" +<meta name="viewport" content="width=device-width, initial-scale=1.0">

--- index.php
+++ index.php
@@ -23,12 +23,13 @@ defined('_JEXEC') or die;
 header("Content-Type: text/text/html; charset=utf-8");
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
 	<meta charset="UTF-8" />
 	<title>Интересные растения</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 	<link rel="stylesheet" type="text/css" href="style/style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
