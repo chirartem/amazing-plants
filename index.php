dc075445eb5886f837bf2a25d01a955a5f813c66
Artem Chirtsov <chirartem@gmail.com>
2021-03-21 13:44:38 +0300

    renew

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
@@ -58,14 +60,14 @@ if ($conn->connect_error) {
 			</ul>
 		</div>
 	</div>
-	<main>
+	<div id="content">
 		<?php
-		if (isset($_GET['type']))
+                if (isset($_GET['type']))
 			$link_type = $_GET["type"];
 		else
 			$link_type = 0;
 
-		if (isset($_GET['id']))
+                if (isset($_GET['id']))
 			$link_id = $_GET["id"];
 		else
 			$link_id = 0;
@@ -113,7 +115,7 @@ if ($conn->connect_error) {
 			<?php 
 		} 
 		?>
-	</main>
+	</div>
 	<footer></footer>
 	<a href="#" id="back-to-top"></a>
 	<script src="script/script.js"></script>
