ca92b6c6e8d0751d942cdd0e8e065994dc1fa0e9
Artem Chirtsov <chirartem@gmail.com>
2021-03-21 15:14:13 +0300

    ТЕСТ табличное представление

--- index.php
+++ index.php
@@ -30,91 +30,93 @@ if ($conn->connect_error) {
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
-	<header></header>
-	<div class="display-flex">
-		<div id="menu">
-			<h2>Растения:</h2>
-			<ul>
+	<header><h1></h1></header>
+	<div id="tableContainer">
+		<div id="tableRow">
+			<div id="menu">
+				<h2>Растения:</h2>
+				<ul>
 
-				<?php
-				$sql = "SELECT id, name from plants where visible=1 order by sort";
-				$result = $conn->query($sql);
+					<?php
+					$sql = "SELECT id, name from plants where visible=1 order by sort";
+					$result = $conn->query($sql);
 
-				while($row = $result->fetch_assoc()) {
-					echo "<li><a href=\"/?type=plants&id=".$row["id"]."\">".$row["name"]."</a></li>";
-				} 
-				?>
-			</ul>
-			<h2>Деревья:</h2>
-			<ul>
+					while($row = $result->fetch_assoc()) {
+						echo "<li><a href=\"/?type=plants&id=".$row["id"]."\">".$row["name"]."</a></li>";
+					} 
+					?>
+				</ul>
+				<h2>Деревья:</h2>
+				<ul>
 
-				<?php
-				$sql = "SELECT id, name from trees where visible=1 order by sort";
-				$result = $conn->query($sql);
+					<?php
+					$sql = "SELECT id, name from trees where visible=1 order by sort";
+					$result = $conn->query($sql);
 
-				while($row = $result->fetch_assoc()) {
-					echo "<li><a href=\"/?type=trees&id=".$row["id"]."\">".$row["name"]."</a></li>";
-				} 
-				?>
+					while($row = $result->fetch_assoc()) {
+						echo "<li><a href=\"/?type=trees&id=".$row["id"]."\">".$row["name"]."</a></li>";
+					} 
+					?>
 
-			</ul>
-		</div>
-	</div>
-	<div id="content">
-		<?php
-                if (isset($_GET['type']))
-			$link_type = $_GET["type"];
-		else
-			$link_type = 0;
-
-                if (isset($_GET['id']))
-			$link_id = $_GET["id"];
-		else
-			$link_id = 0;
-
-		if (($link_type) && ($link_id)) {
-			$sql = "SELECT name, content from $link_type where visible=1 and id=$link_id";
-			$result = $conn->query($sql);
-
-			$row = $result->fetch_assoc();
-			echo $row["name"];
-			echo "<br />";
-			echo $row["content"];
-			//echo "<img src='/images/{$link_type}_{$link_id}.jpg'>"
-		}
-		?>
-		<?php
-		if ($link_id == 0) {
-			?>
-			<div class="cards">
-				<div class="flex-wrap card" style="width: 18rem;">
-					<img src="images/card-avatars/icon_1.png" class="card-img-top" alt="Самые большие">
-					<div class="card-body">
-						<div class="button-align-card"><a href="#" class="btn btn-primary">Самые большие</a></div>
-					</div>
-				</div>
-				<div class="flex-wrap card" style="width: 18rem;">
-					<img src="images/card-avatars/icon_3.png" class="card-img-top" alt="Самые маленькие">
-					<div class="card-body">
-						<div class="button-align-card"><a href="#" class="btn btn-primary">Самые маленькие</a></div>
-					</div>
-				</div>
-				<div class="flex-wrap card" style="width: 18rem;">
-					<img src="images/card-avatars/icon_3.png" class="card-img-top" alt="Самые маленькие">
-					<div class="card-body">
-						<div class="button-align-card"><a href="#" class="btn btn-primary">Самые маленькие</a></div>
-					</div>
-				</div>
-				<div class="flex-wrap card" style="width: 18rem;">
-					<img src="images/card-avatars/icon_3.png" class="card-img-top" alt="Самые маленькие">
-					<div class="card-body">
-						<div class="button-align-card"><a href="#" class="btn btn-primary">Самые маленькие</a></div>
-					</div>
-				</div>
+				</ul>
 			</div>
-			<?php 
-		} 
-		?>
+			<main>
+				<?php
+				if (isset($_GET['type']))
+					$link_type = $_GET["type"];
+				else
+					$link_type = 0;
+
+				if (isset($_GET['id']))
+					$link_id = $_GET["id"];
+				else
+					$link_id = 0;
+
+				if (($link_type) && ($link_id)) {
+					$sql = "SELECT name, content from $link_type where visible=1 and id=$link_id";
+					$result = $conn->query($sql);
+
+					$row = $result->fetch_assoc();
+					echo $row["name"];
+					echo "<br />";
+					echo $row["content"];
+			//echo "<img src='/images/{$link_type}_{$link_id}.jpg'>"
+				}
+				?>
+				<?php
+				if ($link_id == 0) {
+					?>
+					<div class="cards">
+						<div class="flex-wrap card" style="width: 18rem;">
+							<img src="images/card-avatars/icon_1.png" class="card-img-top" alt="Самые большие">
+							<div class="card-body">
+								<div class="button-align-card"><a href="#" class="btn btn-primary">Самые большие</a></div>
+							</div>
+						</div>
+						<div class="flex-wrap card" style="width: 18rem;">
+							<img src="images/card-avatars/icon_3.png" class="card-img-top" alt="Самые маленькие">
+							<div class="card-body">
+								<div class="button-align-card"><a href="#" class="btn btn-primary">Самые маленькие</a></div>
+							</div>
+						</div>
+						<div class="flex-wrap card" style="width: 18rem;">
+							<img src="images/card-avatars/icon_3.png" class="card-img-top" alt="Самые маленькие">
+							<div class="card-body">
+								<div class="button-align-card"><a href="#" class="btn btn-primary">Самые маленькие</a></div>
+							</div>
+						</div>
+						<div class="flex-wrap card" style="width: 18rem;">
+							<img src="images/card-avatars/icon_3.png" class="card-img-top" alt="Самые маленькие">
+							<div class="card-body">
+								<div class="button-align-card"><a href="#" class="btn btn-primary">Самые маленькие</a></div>
+							</div>
+						</div>
+					</div>
+					<?php 
+				} 
+				?>
+			</main>
+		</div>
 	</div>
 	<footer></footer>
 	<a href="#" id="back-to-top"></a>
