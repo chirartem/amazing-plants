dad09594fa34cdaaff394798a7956130794cdff2
Artem Chirtsov <chirartem@gmail.com>
2021-03-21 18:12:54 +0300

    -link bootstrap +h2 "font-size: 14;"

--- index.php
+++ index.php
@@ -22,9 +22,9 @@ if ($conn->connect_error) {
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+	<link rel="stylesheet" href="style/nullstellen.css">
 	<meta charset="UTF-8" />
 	<title>Интересные растения</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 	<link rel="stylesheet" type="text/css" href="style/style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
